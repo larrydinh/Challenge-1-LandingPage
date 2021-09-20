f2d52b15307d77bf865dd01383eb8ab0aa12eb97
Larry <huuphucdinh@gmail.com>
2021-09-20 21:14:56 +0700

    day2

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,30 @@
-const btnHamburger = document.querySelector(`#btnHamburger`);
-btnHamburger.addEventListener('click', function () {
-    console.log("click hamburger");
-    if (btnHamburger.classList.contains('open')) {
-        btnHamburger.classList.remove('open')
+// const btnHamburger = document.querySelector(`#btnHamburger`);
+const header = document.getElementById('header');
+const overlay = document.querySelector('.overlay');
+const noscroll = document.querySelector('.noscroll');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+
+header.addEventListener('click', function () {
+    console.log("click header");
+    if (header.classList.contains('open')) { //Close Hamburger menu 
+
+        noscroll.classList.remove('noscroll')
+        header.classList.remove('open');
+        fadeElements.forEach(function (element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        })
+
     }
-    else {
-        btnHamburger.classList.add('open')
+    else { //Open Hamburger menu
+        noscroll.classList.add('noscroll')
+        header.classList.add('open')
+        fadeElements.forEach(function (element) {
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+   
+
+        })
     }
 })
- 
